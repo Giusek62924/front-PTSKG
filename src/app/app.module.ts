a70cee5b420d5ab83830e93bfcb415965fc31d8a
Giuseppe Kaligare TSG <desarrollo8@tsg.net.co>
2022-09-25 21:55:25 -0500

    edee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ToastrModule } from "ngx-toastr";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { BsModalRef, ModalOptions } from "ngx-bootstrap/modal";
+
+import { Servers } from "./servers.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/servers.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+export const Servers = {
+  Development : "http://34.102.36.174:8000",
+  Deploy : "http://localhost:8000", // Cygnus: https://api.cygnus.tsg.net.co Tel: http://172.30.131.195:8000
+  Broker_Deploy : "172.30.131.193"    // Cygnus : broker.prod.cygnus.tsg.net.co Tel : 172.30.131.193
+}
